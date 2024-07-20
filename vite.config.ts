d6401840e80dd5226c8ef423378f99379818a07e
tigawanna <denniskinuthiaw@gmail.com>
2024-07-20 17:44:44 +0300

    endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import vercel from "vite-plugin-vercel";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import vitePluginVercelApi from "vite-plugin-vercel-api";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react(), vercel(), vitePluginVercelApi()],
+
 });
