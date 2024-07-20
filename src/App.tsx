dcd23483dbacd44f73142dc17c73fdf0a851e2c5
tigawanna <denniskinuthiaw@gmail.com>
2024-07-20 16:44:14 +0300

    eslint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  return (
+return (
     <div className="w-full h-full">
       <h1>Hello</h1>
     </div>
